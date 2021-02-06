94c0d8170075ff1cd24ee0ec0e6fb73f98d515f1
Stanisław Światłoch <sswiatloch@hotmail.com>
2021-02-06 12:24:58 +0100

    basic structure

---
+++ time_profiler.py
@@ -0,0 +1,25 @@
+class TimeProfiler:
+    def get_instance(self):
+        pass
+
+    def register_connection(self):
+        pass
+
+    def reqister_time(self, func_name, time, type):
+        pass
+
+
+class TimeQuerry:
+    def __init__(self, func):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class TimeExecution:
+    def __init__(self, func):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
