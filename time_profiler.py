f906f01df019b184ccdae18e595197221eefc8bf
januszgo <62623687+januszgo@users.noreply.github.com>
2021-02-08 19:36:59 +0100

    Add files via upload

--- time_profiler.py
+++ time_profiler.py
@@ -15,29 +15,45 @@ class DatabaseFactory:
         elif dbtype == DBTypes.MYSQL:
             return db.MysqlDB(conn)
 
+class TimeProfilerMeta(type):
+    
+    instances={}
 
-class TimeProfiler:
-    def get_instance(self):
+    def __call__(cls,*args,**kwargs):
+        if cls not in cls.instances:
+            instance = super().__call__(*args,**kwargs)
+            instance.database=DatabaseFactory.create()
+            cls.instances[cls]=instance
+        return cls.instances[cls]
+
+class TimeProfiler(metaclass=TimeProfilerMeta):
+
+    def register_connection(self,type, conn):
+        return DatabaseFactory.create(conn,type)
+
+    def reqister_time(self, func_name, time, type):
         pass
-
-    def register_connection(self):
+    
+    def show_logs():
         pass
-
-    def reqister_time(self, func_name, time, dbtype):
-        pass
-
 
 class TimeQuerry:
     def __init__(self, func):
-        pass
-
+        self.func=func
+        self.num=0
+        
+        
     def __call__(self, *args, **kwargs):
-        pass
-
+        self.num+=1
+        self.prof=TimeProfiler()
+        
 
 class TimeExecution:
     def __init__(self, func):
-        pass
+        self.func=func
+        self.num=0
+        
 
     def __call__(self, *args, **kwargs):
-        pass
+        self.num+=1
+        self.prof=TimeProfiler()
